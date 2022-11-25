6818530d8dcb9f698fac7e4819084ba03b8e1813
Ohanyan Araqsya <araks1009@gmail.com>
2022-11-25 20:13:19 +0400

    tasks

---
+++ codesignal1.ts
@@ -0,0 +1,12 @@
+// 1
+// let solution = (param1: number, param2: number): number => param1 + param2;
+
+//2
+// function solution(year: number): number {
+//     return Math.ceil(year/100);
+// }
+//
+// //3
+// function solution(inputString: string): boolean {
+//     return inputString === inputString.split("").reverse().join("");
+// }
